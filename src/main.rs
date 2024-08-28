ae22ed534cf543bf077197778c97ca9c04bd3e2f
codaishin <codaishin@gmail.com>
2024-08-28 17:23:54 +0200

    feat: fuel and take off

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use love_hate_love::spaceship::Spaceship;
 
 fn main() {
-	let _spaceship = Spaceship { name: "Defiant" };
+	let mut spaceship = Spaceship {
+		name: "Defiant",
+		..Default::default()
+	};
+	spaceship.fuel();
+	spaceship.take_off();
 }
--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,3 +1,23 @@
+#[derive(Default)]
 pub struct Spaceship {
 	pub name: &'static str,
+	pub state: State,
+}
+
+#[derive(Default)]
+pub enum State {
+	#[default]
+	UnFueled,
+	Fueled,
+	InSpace,
+}
+
+impl Spaceship {
+	pub fn fuel(&mut self) {
+		self.state = State::Fueled;
+	}
+
+	pub fn take_off(&mut self) {
+		self.state = State::InSpace;
+	}
 }
