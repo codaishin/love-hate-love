9e19fc408a4fa7c55fab67d8a5498a66014349b1
codaishin <codaishin@gmail.com>
2024-08-28 18:00:53 +0200

    feat: collision

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use love_hate_love::spaceship::Spaceship;
+use love_hate_love::spaceship::{Destroyed, Spaceship};
 
-fn main() {
+fn main() -> Result<(), Destroyed> {
 	let spaceship = Spaceship::new("Defiant");
 	let spaceship = spaceship.fuel();
 	let spaceship = spaceship.take_off();
+	let spaceship = match spaceship.collision_damage(16) {
+		Ok(spaceship) => spaceship,
+		Err(destroyed) => return Err(destroyed),
+	};
 	let _spaceship = spaceship.land();
+
+	Ok(())
 }
--- src/spaceship.rs
+++ src/spaceship.rs
@@ -3,6 +3,7 @@ use std::marker::PhantomData;
 #[derive(Default)]
 pub struct Spaceship<TState> {
 	pub name: &'static str,
+	hit_points: usize,
 	state: PhantomData<TState>,
 }
 
@@ -10,36 +11,48 @@ pub struct UnFueled;
 pub struct Fueled;
 pub struct InSpace;
 
+fn translate<TIn, TOut>(spaceship: Spaceship<TIn>) -> Spaceship<TOut> {
+	Spaceship {
+		name: spaceship.name,
+		hit_points: spaceship.hit_points,
+		state: PhantomData,
+	}
+}
+
 impl Spaceship<UnFueled> {
 	pub fn new(name: &'static str) -> Self {
 		Self {
 			name,
+			hit_points: 100,
 			state: PhantomData,
 		}
 	}
 
 	pub fn fuel(self) -> Spaceship<Fueled> {
-		Spaceship {
-			name: self.name,
-			state: PhantomData,
-		}
+		translate(self)
 	}
 }
 
 impl Spaceship<Fueled> {
 	pub fn take_off(self) -> Spaceship<InSpace> {
-		Spaceship {
-			name: self.name,
-			state: PhantomData,
-		}
+		translate(self)
 	}
 }
 
+#[derive(Debug)]
+pub struct Destroyed;
+
 impl Spaceship<InSpace> {
 	pub fn land(self) -> Spaceship<UnFueled> {
-		Spaceship {
-			name: self.name,
-			state: PhantomData,
+		translate(self)
+	}
+
+	pub fn collision_damage(mut self, damage: usize) -> Result<Spaceship<InSpace>, Destroyed> {
+		if damage > self.hit_points {
+			Err(Destroyed)
+		} else {
+			self.hit_points -= damage;
+			Ok(translate(self))
 		}
 	}
 }
